37435253b8628b74d1b66a0270e3dd041695e8ce
Joshua Barber <58489546+jbarberrrr@users.noreply.github.com>
2021-11-29 17:59:29 -0600

    Add files via upload

--- team3Project/GamePanel.java
+++ team3Project/GamePanel.java
@@ -9,40 +9,48 @@ public class GamePanel extends JPanel
     private JButton hit;
     private JButton stand;
     private JButton quit; 
-    private JLabel win; 
-    private JLabel lose; 
+    public JLabel status;
+    public JLabel playerScore; 
+    private JLabel dealerScore; 
 
     //Constructor for Game panel
     public GamePanel(GameControl gc)
     {
+    	// creates status that is updated by control
+        status = new JLabel("Your Score: ", JLabel.CENTER);
+        playerScore = new JLabel("Your Score: 0", JLabel.LEFT);
+        dealerScore = new JLabel("Dealer Score: 0", JLabel.LEFT);
+    	
         //Creates hit/stand/quit buttons
         hit = new JButton("Hit");
         hit.addActionListener(gc);
-        JPanel hitBuffer = new JPanel(); 
-        hitBuffer.add(hit);
-
         stand = new JButton("Stand");
         stand.addActionListener(gc);
-        JPanel standBuffer = new JPanel();
-        standBuffer.add(stand);
-
         quit = new JButton("Quit");
         quit.addActionListener(gc);
-        JPanel quitBuffer = new JPanel();
-        quitBuffer.add(quit);
 
-        //Creates win/lose labels
-        win = new JLabel("Win", JLabel.CENTER);
-        lose = new JLabel("Lose", JLabel.CENTER);
+        // create panel for the status
+        JPanel labelsTop = new JPanel();
+        labelsTop.add(status);
+
+        // create panel for game field
+        JPanel gameField = new JPanel();
+        gameField.setBackground(Color.green);
+        gameField.add(dealerScore);
+        gameField.add(playerScore);
+
+        // create panel for buttons
+        JPanel buttonsBottom = new JPanel(new GridLayout(1, 3, 5, 5));
+        buttonsBottom.add(hit);
+        buttonsBottom.add(stand);
+        buttonsBottom.add(quit);
 
         //Arrange components on the Panel
         JPanel grid = new JPanel(new BorderLayout());
-        grid.add(hit, BorderLayout.SOUTH);
-        grid.add(stand, BorderLayout.SOUTH);
-        grid.add(quit, BorderLayout.NORTH);
-        grid.add(win, BorderLayout.CENTER);
-        grid.add(lose, BorderLayout.CENTER);
+        grid.setPreferredSize(new Dimension(500,325));    //Added to set the size of the gamefield 
+        grid.add(labelsTop, BorderLayout.NORTH);
+        grid.add(gameField, BorderLayout.CENTER);
+        grid.add(buttonsBottom, BorderLayout.SOUTH);
         this.add(grid);
-
     }
 }
