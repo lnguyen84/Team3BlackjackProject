4e9a83b43739ac7832e52a91eaee62ee57e4e8b0
Ewelch4 <60515100+Ewelch4@users.noreply.github.com>
2021-11-29 16:35:43 -0600

    Add files via upload

--- team3Project/InitialControl.java
+++ team3Project/InitialControl.java
@@ -3,16 +3,18 @@ package team3Project;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.IOException;
 
 public class InitialControl implements ActionListener
 {
 	// Private data field for storing the container.
 	private JPanel container;
-
+	private ChatClient client;
 	// Constructor for the initial controller.
-	public InitialControl(JPanel container)
+	public InitialControl(JPanel container, ChatClient client)
 	{
 		this.container = container;
+		this.client = client;//adding client features to allow for scaling
 	}
 
 	// Handle button clicks.
@@ -38,5 +40,30 @@ public class InitialControl implements ActionListener
 		      CardLayout cardLayout = (CardLayout)container.getLayout();
 		      cardLayout.show(container, "3");
 		    }
+		 //added controls for connect button
+		 else if (command.equals("Connect")){
+			 try
+				{
+					client.openConnection();
+				} catch (IOException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		 }
+		 //
+		 else if(ae.getSource() instanceof JMenuItem) {
+			  String host = JOptionPane.showInputDialog("Enter IP: ", "localhost");
+			  if(!(host == null)) {
+				  client.setHost(host);
+			  }
+			  String port =JOptionPane.showInputDialog("Enter Port: ",8300);
+			  int realport = Integer.parseInt(port);
+			  if (!(port == null)) {
+				  client.setPort(realport);
+			  }
+		  }
+		 
+		 
 	}
 }
