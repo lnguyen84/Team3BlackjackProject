6fb4a515eb180ba79002dac651fa01b5aefa9eec
Joshua Barber <58489546+jbarberrrr@users.noreply.github.com>
2021-11-30 01:46:35 -0600

    Add files via upload

--- team3Project/GamePanel.java
+++ team3Project/GamePanel.java
@@ -19,12 +19,15 @@ public class GamePanel extends JPanel
 	private player dealer;
 	private deck Deck;
 	private card Card;
+	private int games;
 
 	//Constructor for Game panel
 	public GamePanel(GameControl gc)
 	{
+
 		// creates status that is updated by control
-		status = new JLabel("", JLabel.CENTER);
+		games = 1;
+		status = new JLabel("Game " + games, JLabel.CENTER);
 		playerScore = new JLabel(" Your Score: 0", JLabel.LEFT);
 		dealerScore = new JLabel(" Dealer Score: 0", JLabel.LEFT);
 		playerScore.setFont(new Font("Times New Roman", Font.PLAIN, 18));
@@ -86,13 +89,14 @@ public class GamePanel extends JPanel
 
 	private void Play() {
 		//System.out.println("Listen Button Pressed");
+		status.setText("Game " + games++);
 		play.setVisible(false);
 		//gc.start();
 		player1 = new player();
 		dealer = new player();
 		Deck = new deck();
 		Deck.shuffle();
-		for(int i=0;i<2;i++)
+		for(int i=0;i<2;i++)//give dealer & player first 2 cards
 		{
 			try {
 				Card = Deck.nextCard();
@@ -111,7 +115,7 @@ public class GamePanel extends JPanel
 
 		}
 		playerScore.setText(" Your Score: " + player1.hand.calculateTotal());
-		dealerScore.setText(" Dealer Score: " + dealer.hand.calculateTotal());
+		dealerScore.setText(" Dealer Score: " + dealer.hand.calculateTotal());//shows innitial scores
 	}
 
 	private void Hit() {
@@ -123,9 +127,17 @@ public class GamePanel extends JPanel
 		}
 		playerScore.setText(" Your Score: " + player1.hand.calculateTotal());
 		if(player1.hand.calculateTotal() > 21)
+		{
 			status.setText("You busted, you lose!");
+			play.setVisible(true);
+			play.setText("Play Again");
+			if(play.isSelected())
+			{
+				Play();
+			}
+		}
 	}
-	
+
 	private void Stand() {
 		while(dealer.hand.calculateTotal() < 17)
 		{
@@ -138,11 +150,19 @@ public class GamePanel extends JPanel
 		}
 		playerScore.setText(" Your Score: " + player1.hand.calculateTotal());
 		dealerScore.setText(" Dealer Score: " + dealer.hand.calculateTotal());
-		if(player1.hand.calculateTotal() > dealer.hand.calculateTotal() && player1.hand.calculateTotal() <= 21 && dealer.hand.calculateTotal() <= 21)
+		if(player1.hand.calculateTotal() > dealer.hand.calculateTotal() && player1.hand.calculateTotal() <= 21)
+			status.setText("You Win!");
+		else if(player1.hand.calculateTotal() <= 21 && dealer.hand.calculateTotal() > 21)
 			status.setText("You Win!");
 		else
 			status.setText("You Lose!");
-		//PlayAgain.setVisible();
+
+		play.setVisible(true);
+		play.setText("Play Again");
+		if(play.isSelected())
+		{
+			Play();
+		}
 	}
 
 }
