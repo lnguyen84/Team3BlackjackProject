c41c79bcf77f6ea3aaed4c822ca7cae11d4093de
b-barrientos <49875686+b-barrientos@users.noreply.github.com>
2021-11-29 23:58:20 -0600

    Added Rules information to ViewRulesPanel

--- team3Project/ViewRulesPanel.java
+++ team3Project/ViewRulesPanel.java
@@ -9,13 +9,14 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class ViewRulesPanel extends JPanel
 {
 	private JButton back;
 	private JButton logout;
-	private JTextField RulesArea;
+	private JTextArea RulesArea;
 
 	private ChatClient client;
 
@@ -26,7 +27,17 @@ public class ViewRulesPanel extends JPanel
 		labelPanel.add(label);
 		
 		//Create the area for user's contacts
-		RulesArea = new JTextField("Rules go here");
+		RulesArea = new JTextArea("Blackjack is a well-known card game that is a \n"
+				+ "standard game for all casinos.\n"
+				+ "Each game can consist of 1 to 5 players and 1 dealer.\n"
+				+ "They will be using a standard deck of cards that consists \n"
+				+ "of 52 cards, where the ace can be counted as 1 or 11, \n"
+				+ "while face cards are 10. The game starts with the dealer \n"
+				+ "shuffling and dealing 2 cards to every player including themselves,\n"
+				+ "the players can ask for more cards from the dealer if \n"
+				+ "necessary to reach the goal of 21 but not over 21.\n"
+				+ "In order to win, the player's cards must be 21 or closer \n"
+				+ "when compared to the dealer’s cards.\n");
 		RulesArea.setEditable(false);
 		JScrollPane scroll = new JScrollPane(RulesArea);
 		scroll.setPreferredSize(new Dimension(400,200));
