a0e48676030abaf3c2d4cbf77309bbeaf771a9d9
bmyers3 <72058626+bmyers3@users.noreply.github.com>
2021-11-30 13:58:27 -0600

    Add files via upload

--- team3Project/HandTest.java
+++ team3Project/HandTest.java
@@ -6,24 +6,24 @@ import org.junit.Test;
 
 public class HandTest {
 
-	@Test
-	public void addCardtest() {
-		Hand h1 = new Hand();
-		card c1 = new card();
-		deck d1 = new deck();
-		
-		d1.shuffle();
-		try {
-			c1 = d1.nextCard();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		h1.addCard(c1);
-		assertEquals(h1.getCard(1),c1);
-		//if(assertEquals(h1.getCard(1),c1))
-		//	System.out.ln("pass");
-	}
+    @Test
+    public void addCardtest() {
+        Hand h1 = new Hand();
+        card c1 = new card();
+        deck d1 = new deck();
+
+        d1.shuffle();
+        try {
+            c1 = d1.nextCard();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        h1.addCard(c1);
+        System.out.println(h1.getCard(0)+","+c1);
+        assertEquals(h1.getCard(0),c1);
+      
+    }
 
 }
